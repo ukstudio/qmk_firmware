514eb1219d0384435217bd87bbf0c0197bb1de1d
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2018-08-20 17:45:59 +0900

    Switch GUI and Ctrl

--- keyboards/fortitude60/keymaps/ukstudio/keymap.c
+++ keyboards/fortitude60/keymaps/ukstudio/keymap.c
@@ -37,17 +37,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * | =    |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  | GUI  | Alt  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  | Ctrl | Alt  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |  `   | Ctrl | Bksp |Shift | Ctrl |Enter |Space |Raise |  [   |  ]   |
+ *               |  `   | Ctrl | Bksp |Shift | GUI  |Enter |Space |Raise |  [   |  ]   |
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
   KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   KC_EQUAL,  KC_A,  KC_S,    KC_D,   KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_LGUI,  KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-                KC_GRAVE, KC_LALT,  KC_BSPC,  KC_LSFT, KC_LCTL,  KC_ENT,  KC_SPC,  RAISE,   KC_LBRC, KC_RBRC \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_LCTL,  KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
+                KC_GRAVE, KC_LALT,  KC_BSPC,  KC_LSFT, KC_LGUI,  KC_ENT,  KC_SPC,  RAISE,   KC_LBRC, KC_RBRC \
 ),
 
 /* Lower
