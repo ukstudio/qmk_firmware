f3e3273a4ee27ab7b6a9bc55aac27ad0438305fc
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2018-08-23 13:44:26 +0900

    update keymap.c

--- keyboards/fortitude60/keymaps/ukstudio/keymap.c
+++ keyboards/fortitude60/keymaps/ukstudio/keymap.c
@@ -30,24 +30,24 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-----------------------------------------.             ,-----------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  -   |
- * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  ]   |
- * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | =    |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  [   |
- * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  | Alt  | RAISE|   N  |   M  |   ,  |   .  |   /  |Shift |
- * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |  `   | Shift|Lower| Ctrl | GUI  |Enter |Space |       |  '   |  \   |
+ * ,-------------------------------------------.             ,-----------------------------------------.
+ * | Esc  |   1  |   2    |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+--------+------+------+------|             |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W    |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  [   |
+ * |------+------+--------+------+------+------|             |------+------+------+------+------+------|
+ * | =    |   A  |   S    |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  ]   |
+ * |------+------+--------+------+------+------+-------------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X    |   C  |   V  |   B  | Alt  | Bksp |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * `-------------+--------+------+------+------+------+------+------+------+------+------+-------------'
+ *               |ctrl+alt|  `   | Lower| Ctrl | GUI  |Enter |Space |Raise |  '   |  \   |
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, \
-  KC_EQUAL,  KC_A,  KC_S,    KC_D,   KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LBRC, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_LALT,  RAISE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-                KC_GRAVE,  KC_LSFT,  LOWER,   KC_LCTL, KC_LGUI,  KC_ENT,  KC_SPC,  KC_RALT, KC_QUOT, KC_BSLASH\
+  KC_ESC,  KC_1,    KC_2, KC_3,     KC_4,  KC_5,                       KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINUS, \
+  KC_TAB,  KC_Q,    KC_W, KC_E,     KC_R,  KC_T,                       KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, \
+  KC_EQUAL,  KC_A,  KC_S, KC_D,     KC_F,  KC_G,                       KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_RBRC, \
+  KC_LSFT, KC_Z,    KC_X, KC_C,     KC_V,  KC_B,    KC_LALT,  KC_BSPC, KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
+          LALT(KC_LCTRL), KC_GRAVE, LOWER, KC_LCTL, KC_LGUI,  KC_ENT,  KC_SPC,  RAISE, KC_QUOT, KC_BSLASH\
 ),
 
 /* Lower
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+----------+------+------+------+-------------+------+------+------+------+------+------|
  * |      |  F7  |  F8      |  F9  |  F10 |  F11 |      |      |  F12 |ISO ~ |ISO | |      |      |      |
  * `-------------+----------+------+------+------+------+------+------+------+------+------+-------------'
- *               |          |      |      |      |      |      | Bksp |      |      |      |
+ *               |          |      |      |      |      |      |      |      |      |      |
  *               `-------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,         KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLASH,  \
   KC_DEL,  KC_F1,   LALT(KC_DOWN), KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_QUOT, \
   _______, KC_F7,   KC_F8,         KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______,\
-                    _______,      _______, _______, _______, _______, _______,  KC_BSPC, _______, _______, _______\
+                    _______,      _______, _______, _______, _______, _______,  _______, _______, _______, _______\
 ),
 
 /* Raise
